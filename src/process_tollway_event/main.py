d90e79800358bd5325f12a9af30cfe7bd2350488
Ben Griffith <bengriffith@outlook.com>
2024-03-26 13:44:22 +0000

    updated dependencies

--- src/process_tollway_event/main.py
+++ src/process_tollway_event/main.py
@@ -1,6 +1,12 @@
 import base64
+import os
+
+from google.cloud import redis_v1
+import redis
+
+REDIS_HOST = os.environ.get("REDIS_HOST")
+REDIS_PORT = os.environ.get("REDIS_PORT")
 
 
 def process_tollway_traffic(data, context):
     tollway_event = base64.b64decode(data["data"]).decode("utf-8")
-    print(f"tollway event: {tollway_event}")
