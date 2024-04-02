836c3cbad52a80c704da3d3d1b6aff940ca5260e
Ben Griffith <bengriffith@outlook.com>
2024-04-02 14:02:47 +0000

    started on insert for fact_tollway_event

--- src/cloud_run/main.py
+++ src/cloud_run/main.py
@@ -1,4 +1,6 @@
+from typing import NamedTuple
 import json
+import uuid
 
 from google.cloud import pubsub_v1, bigquery
 
@@ -12,5 +14,30 @@ from src.cloud_run.constants import (
 subscriber_client = pubsub_v1.SubscriberClient()
 bigquery_client = bigquery.Client()
 
+class TollwayEvent(NamedTuple):
+    year: str
+    make: str
+    model: str
+    category: str
+    license_plate: str
+    vin: str
+    state: str
+    primary_color: str
+    tollway_state: str
+    tollway_name: str
+    timestamp: str
+
+
 def messages_to_bigquery(messages):
-    ...
+    rows_to_insert = []
+
+    for message in messages:
+        message_data = json.loads(message.data.decode("utf-8"))
+        message_data = TollwayEvent(**message_data)
+
+    fact_tollway_event_row = {
+        "event_id": str(uuid.uuid4()),
+        "vehicle_id": "",
+        "tollway_id": "",
+        "timestamp": ""
+    }
