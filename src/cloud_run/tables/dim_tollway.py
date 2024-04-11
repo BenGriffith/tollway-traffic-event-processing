f88c4f5fd7215324fdea0a64796409e48468f16e
Ben Griffith <bengriffith@outlook.com>
2024-04-11 13:28:03 +0000

    added check to see if value exists before inserting

--- src/cloud_run/tables/dim_tollway.py
+++ src/cloud_run/tables/dim_tollway.py
@@ -1,4 +1,5 @@
 from constants import DATASET_ID, PROJECT_ID, TABLES
+from table_logging import table_logger
 from utils import hash_string
 
 
@@ -13,14 +14,12 @@ def get_tollway_names(bigquery_client):
 
 
 def insert_row(bigquery_client, message_data):
-    # check to see if tollway does not exist in table
-    # if it does return
-    # else continue
 
     tollway_names = get_tollway_names(bigquery_client)
 
     if message_data.tollway_name in tollway_names:
-        # logger
+        tollway_logger = table_logger.setup_logger(True)
+        tollway_logger.info(f"{message_data.tollway_name} exists in dim_tollway")
         return
 
     dim_tollway_row = {
@@ -30,9 +29,8 @@ def insert_row(bigquery_client, message_data):
 
     table_ref = bigquery_client.dataset(DATASET_ID).table(TABLES["dim_tollway"])
     dim_tollway_errors = bigquery_client.insert_rows_json(table_ref, [dim_tollway_row])
+
     if dim_tollway_errors:
         ...
-        # implement logger
     else:
         ...
-        # implement logger
