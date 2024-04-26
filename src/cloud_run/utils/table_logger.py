7feb3080ee2a1142c423dae18840e535318216ea
Ben Griffith <bengriffith@outlook.com>
2024-04-26 22:31:00 +0000

    issue #18

--- src/cloud_run/utils/table_logger.py
+++ src/cloud_run/utils/table_logger.py
@@ -2,19 +2,19 @@ import logging
 
 
 def setup_logger(enable=False, name="table", level=logging.INFO, log_file="table.log"):
-    if enable:
-        table_logger = logging.getLogger(name)
-        table_logger.setLevel(level)
-
-        if not table_logger.handlers:
-            file_handler = logging.FileHandler(log_file)
-            file_handler.setLevel(level)
-
-            formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-            file_handler.setFormatter(formatter)
-
-            table_logger.addHandler(file_handler)
-
-        return table_logger
-    else:
+    if not enable:
         return logging.getLogger("null")
+
+    table_logger = logging.getLogger(name)
+    table_logger.setLevel(level)
+
+    if not table_logger.handlers:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(level)
+
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        file_handler.setFormatter(formatter)
+
+        table_logger.addHandler(file_handler)
+
+    return table_logger
