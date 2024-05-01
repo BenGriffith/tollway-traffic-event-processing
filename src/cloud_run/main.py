634cc2a02c80483c3ebca4507d94da157c475441
Ben Griffith <bengriffith@outlook.com>
2024-05-01 01:40:37 +0000

    incremental updates to containerize via cloud run

--- src/cloud_run/main.py
+++ src/cloud_run/main.py
@@ -1,20 +1,58 @@
 import json
 
 from google.cloud import pubsub_v1, bigquery
+from fastapi import FastAPI, BackgroundTasks
 
-from tables import (
-    fact_tollway_event,
-    dim_tollway,
-)
-
-subscriber_client = pubsub_v1.SubscriberClient()
-bigquery_client = bigquery.Client()
+from utils.helpers import TollwayEvent, create_rows
+from utils.constants import TABLES, PROJECT_ID, SUBSCRIPTION_ID
+from utils.table_logger import setup_logger
+from tables.table_manager import TableManager
 
 
-def messages_to_bigquery(bigquery_client, messages):
+app = FastAPI()
 
-    for message in messages:
-        message_data = json.loads(message.data.decode("utf-8"))
 
-        fact_tollway_event.insert_row(bigquery_client, message_data)
-        dim_tollway.insert_row(bigquery_client, message_data)
+def message_to_bigquery(message):
+    bigquery_client = bigquery.Client()
+
+    # row creation for each table
+    message_data = json.loads(message.data.decode("utf-8"))
+    rows = create_rows(message_data=TollwayEvent(**message_data))
+
+    # table manager for each table
+    for table, row in rows.items():
+        table_manager = TableManager(
+            client=bigquery_client,
+            table=TABLES[table],
+            row=row,
+        )
+        check = True if table != "fact_tollway_event" else False
+
+        table_manager.execute(perform_check=check)
+
+
+def process_messages():
+    subscriber_client = pubsub_v1.SubscriberClient()
+    subscription_path = subscriber_client.subscription_path(project=PROJECT_ID, subscription=SUBSCRIPTION_ID)
+
+    def callback(message):
+        tollway_logger = setup_logger(True)
+        tollway_logger.info(f"Received message: {message.data.decode('utf-8')}")
+        message_to_bigquery(message)
+        message.ack()
+
+    streaming_pull = subscriber_client.subscribe(subscription_path, callback)
+    try:
+        streaming_pull.result(timeout=30)
+    except Exception as e:
+        print("Error or timeout occurred", e)
+    finally:
+        streaming_pull.cancel()
+
+    return {"status": "Completed processing messages"}
+
+
+@app.post("/trigger-processing/")
+async def trigger_processing(background_tasks: BackgroundTasks):
+    background_tasks.add_task(process_messages)
+    return {"message": "Processing started"}
--- src/cloud_run/utils/constants.py
+++ src/cloud_run/utils/constants.py
@@ -1,14 +1,24 @@
 import os
 
-PROJECT_ID = "playground-413722"  # os.getenv("PROJECT_ID")
+PROJECT_ID = os.getenv("PROJECT_ID")
 SUBSCRIPTION_ID = os.getenv("SUBSCRIPTION_ID")
-DATASET_ID = "tollway_traffic"  # os.getenv("DATASET_ID")
+DATASET_ID = os.getenv("DATASET_ID")
 TABLES = {
-    "fact_tollway_event": "fact_tollway_event",  # os.getenv("FACT_TOLLWAY_EVENT"),
-    "dim_tollway": "dim_tollway",  # os.getenv("DIM_TOLLWAY"),
+    "fact_tollway_event": os.getenv("FACT_TOLLWAY_EVENT"),
+    "dim_tollway": os.getenv("DIM_TOLLWAY"),
     "dim_vehicle": os.getenv("DIM_VEHICLE"),
     "dim_make": os.getenv("DIM_MAKE"),
     "dim_model": os.getenv("DIM_MODEL"),
     "dim_category": os.getenv("DIM_CATEGORY"),
     "dim_state": os.getenv("DIM_STATE"),
+}
+
+KEYS = {
+    "fact_tollway_event": "event_id",
+    "dim_tollway": "tollway_id",
+    "dim_vehicle": "vehicle_id",
+    "dim_make": "make_id",
+    "dim_model": "model_id",
+    "dim_category": "category_id",
+    "dim_state": "state_id",
 }
